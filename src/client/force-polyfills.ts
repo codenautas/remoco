3042c7995fe8fa82f0cdcb9335a333aa95522543
Emilio Platzer <emilioplatzer@gmail.com>
2023-04-29 19:08:22 -0300

    agrego polyfills

---
+++ src/client/force-polyfills.ts
@@ -0,0 +1,11 @@
+// var mySefl = window;
+
+if (false) { 
+// @ts-ignore
+window["polyfills-bro"] = window["polyfills-bro"] || {}
+// @ts-ignore
+window["polyfills-bro"].Promise = "force";
+// @ts-ignore
+window["polyfills-bro"].fetch = "force";
+
+}
